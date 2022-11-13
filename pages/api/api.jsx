d8f9cda2b3c66317800b426389a3c8a9531534a7
hadi prassetio <hadiprassetio516@gmail.com[C>
2022-11-14 00:30:32 +0700

    update

--- pages/api/api.jsx
+++ pages/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: process.env.REACT_APP_BASEURL,
+  baseURL: " https://hadi-dumbsound.herokuapp.com/api/v1",
 });
 
 export const SetAuthToken = (token) => {
